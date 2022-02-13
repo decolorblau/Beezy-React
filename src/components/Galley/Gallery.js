26e9f004dcc676cb0eec243d0d69ca08f38e14c5
Claudia Font <claudiafontmarques@gmail.com>
2022-02-13 21:42:47 +0100

    Changed gallery variable names for do it general (events, charactes & series)

--- src/components/Galley/Gallery.js
+++ src/components/Galley/Gallery.js
@@ -2,17 +2,21 @@ import Card from "../Card/Card";
 import { NavLink } from "react-router-dom";
 import "./Gallery.scss";
 import PropTypes from "prop-types";
+import { useContext } from "react";
+import MarvelContext from "../../store/contexts/MarvelContext";
 
-const Gallery = ({ events }) => {
+const Gallery = ({ title }) => {
+  const { marvelArray } = useContext(MarvelContext);
+
   return (
-    events && (
+    marvelArray && (
       <>
-        <h2 className="gallery-title">MARVEL EVENTS</h2>
+        <h2 className="gallery-title">MARVEL {title}</h2>
 
         <div className="gallery-container">
-          {events.map((event) => (
-            <NavLink key={event.id} to={`/`}>
-              <Card key={event.id} marvelEvent={event} />
+          {marvelArray.map((item) => (
+            <NavLink key={item.id} to={`/`}>
+              <Card key={item.id} marvelEvent={item} />
             </NavLink>
           ))}
         </div>
@@ -22,7 +26,7 @@ const Gallery = ({ events }) => {
 };
 
 Gallery.propTypes = {
-  events: PropTypes.array.isRequired,
+  marvelArray: PropTypes.array,
 };
 
 export default Gallery;
