2ad59aa8e9b802190421ea6960600f8cce1fe088
Claudia Font <claudiafontmarques@gmail.com>
2022-02-28 22:41:09 +0100

    Created NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,8 +1,12 @@
+import "./NotFoundPage.scss";
+
 const NotFoundPage = () => {
   return (
     <>
-      <div>
-        <h2>NOT FOUND PAGE</h2>
+      <div className="not-found">
+        <h2>404 NOT FOUND PAGE</h2>
+        <p>I don't have this link</p>
+        <button>Go Home</button>
       </div>
     </>
   );
